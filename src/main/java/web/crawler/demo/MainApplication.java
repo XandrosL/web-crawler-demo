b7eba109647dccfc7a58fc529d0e2747876e5e73
glugo <gabrielilugo@gmail.com>
2026-03-17 23:57:24 -0500

    Added feign client and html example

--- src/main/java/web/crawler/demo/MainApplication.java
+++ src/main/java/web/crawler/demo/MainApplication.java
@@ -1,8 +1,10 @@
 package web.crawler.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MainApplication {
 
 }
