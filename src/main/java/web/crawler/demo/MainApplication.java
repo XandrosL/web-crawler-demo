0ae15c0e0a18c67001696d50a4794cb3ba657314
glugo <gabrielilugo@gmail.com>
2026-03-18 00:04:00 -0500

    Revert "Added feign client and html example"
    This reverts commit b7eba109647dccfc7a58fc529d0e2747876e5e73.

--- src/main/java/web/crawler/demo/MainApplication.java
+++ src/main/java/web/crawler/demo/MainApplication.java
@@ -1,10 +1,8 @@
 package web.crawler.demo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class MainApplication {
 
 }
