b908f9529eacff65feb0f8ec791e188964df13a5
glugo <gabrielilugo@gmail.com>
2026-03-23 19:48:46 -0500

    Added Spring Cache and Caffeine dependencies to enable caching of the external call

--- src/main/java/web/crawler/demo/MainApplication.java
+++ src/main/java/web/crawler/demo/MainApplication.java
@@ -3,11 +3,13 @@ package web.crawler.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
 import web.crawler.demo.configuration.AppProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ AppProperties.class })
+@EnableCaching
 public class MainApplication {
 
     public static void main(String[] args) {
